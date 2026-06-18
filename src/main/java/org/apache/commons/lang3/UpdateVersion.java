3fd0f087ad788e485f19ddabc6c1023022584b7f
lau-ka-ho <asdf913@gmail.com>
2026-06-18 19:15:03 +0900

    Initial

---
+++ src/main/java/org/apache/commons/lang3/UpdateVersion.java
@@ -0,0 +1,222 @@
+package org.apache.commons.lang3;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import javax.xml.stream.Location;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
+import org.apache.commons.collections4.IterableUtils;
+import org.apache.commons.lang3.reflect.FieldUtils;
+
+public class UpdateVersion {
+
+	private static class Dependency {
+
+		private String groupId, artifactId, version;
+
+		private Integer versionIndexStart, versionIndexEnd;
+
+	}
+
+	public static void main(final String[] args) throws XMLStreamException, IOException, IllegalAccessException {
+		//
+		final XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+		//
+		final Map<String, String> map = toMap(args);
+		//
+		final String file = map != null ? map.get("file") : null;
+		//
+		final Path path = file != null ? Path.of(file) : null;
+		//
+		final Iterable<String> lines = path != null && isFile(path.toFile()) ? Files.readAllLines(path) : null;
+		//
+		final XMLStreamReader xmlStreamReader = xmlInputFactory != null && path != null && isFile(path.toFile())
+				? xmlInputFactory.createXMLStreamReader(Files.newInputStream(path))
+				: null;
+		//
+		String localName = null;
+		//
+		boolean dependencies = false, exclusions = false;
+		//
+		int event = 0;
+		//
+		Dependency dependency = null;
+		//
+		String line, version = null;
+		//
+		Location location = null;
+		//
+		int indexOf = 0;
+		//
+		while (xmlStreamReader != null && xmlStreamReader.hasNext()) {
+			//
+			if ((event = xmlStreamReader.next()) == XMLStreamConstants.START_ELEMENT) {
+				//
+				if ((Objects.equals(localName = xmlStreamReader.getLocalName(), "dependencies") && !dependencies
+						&& (dependencies = true)) || !dependencies || Objects.equals(localName, "dependency")
+						|| Objects.equals(localName, "scope")
+						|| (Objects.equals(localName, "exclusions") && !exclusions && (exclusions = true)) || exclusions
+						|| (location = xmlStreamReader.getLocation()) == null) {
+					//
+					continue;
+					//
+				} // if
+					//
+			} else if (event == XMLStreamConstants.END_ELEMENT) {
+				//
+				location = xmlStreamReader.getLocation();
+				//
+				if ((Objects.equals(localName = xmlStreamReader.getLocalName(), "dependencies") && dependencies
+						&& !(dependencies = false))) {
+					//
+					break;
+					//
+				} else if (Objects.equals(localName, "scope")
+						|| (Objects.equals(localName, "exclusions") && exclusions && !(exclusions = false))) {
+					//
+					continue;
+					//
+				} else if (Objects.equals(localName, "dependency")) {
+					//
+					if (dependency != null) {
+						//
+						if (map != null && Objects.equals(dependency.groupId, map.get("groupId"))
+								&& Objects.equals(dependency.artifactId, map.get("artifactId"))) {
+							//
+							if (map.containsKey("version")
+									&& !Objects.equals(version = map.get("version"), dependency.version)) {
+								//
+								final StringBuilder sb = new StringBuilder(ObjectUtils.getIfNull(
+										path != null && isFile(path.toFile()) ? Files.readString(path) : null, ""));
+								//
+								if (dependency.versionIndexStart != null && dependency.versionIndexEnd != null) {
+									//
+									sb.delete(dependency.versionIndexStart, dependency.versionIndexEnd);
+									//
+								} // if
+									//
+								System.out.println(String.format("groupId=%1$s,artifactId=%2$s,version=[%3$s->%4$s]",
+										dependency.groupId, dependency.artifactId, dependency.version, version));
+								//
+								Files.writeString(path, dependency.versionIndexStart != null
+										? sb.insert(dependency.versionIndexStart, String.join("", "<version>", version))
+										: sb);
+								//
+							} // if
+								//
+						} // if
+							//
+						dependency = null;
+						//
+					} // if
+						//
+					continue;
+					//
+				} else if (contains(Arrays.asList("groupId", "artifactId", "version"), localName) && dependencies
+						&& !exclusions && (dependency = ObjectUtils.getIfNull(dependency, Dependency::new)) != null) {
+					//
+					FieldUtils.writeDeclaredField(dependency, localName,
+							StringUtils.substringBetween(IterableUtils.get(lines, location.getLineNumber() - 1),
+									StringUtils.join("<", localName, ">"), StringUtils.join("</", localName, ">")),
+							true);
+					//
+				} // if
+					//
+				if (Objects.equals(localName, "version") && dependencies && !exclusions
+						&& (dependency = ObjectUtils.getIfNull(dependency, Dependency::new)) != null) {
+					//
+					if ((line = IterableUtils.get(lines, location.getLineNumber() - 1)) != null) {
+						//
+						dependency.versionIndexStart = (indexOf = line.indexOf("<version>")) >= 0
+								&& line.indexOf("<version>") == line.lastIndexOf("<version>") ? Integer.valueOf(indexOf)
+										: null;
+						//
+						dependency.versionIndexEnd = (indexOf = line.indexOf("</version>")) >= 0
+								&& line.indexOf("</version>") == line.lastIndexOf("</version>")
+										? Integer.valueOf(indexOf)
+										: null;
+						//
+					} // if
+						//
+				} // if
+					//
+			} // if
+				//
+		} // while
+			//
+		if (xmlStreamReader != null) {
+			//
+			xmlStreamReader.close();
+			//
+		} // if
+			//
+	}
+
+	private static boolean contains(final Collection<?> instance, final Object item) {
+		return instance != null && instance.contains(item);
+	}
+
+	private static boolean isFile(final File instance) {
+		return instance != null && instance.getPath() != null && instance.isFile();
+	}
+
+	private static Map<String, String> toMap(final String... ss) {
+		//
+		String s = null;
+		//
+		Map<String, String> map = null;
+		//
+		for (int i = 0; i < length(ss); i++) {
+			//
+			if (Objects.equals(s = ArrayUtils.get(ss, i), "=")) {
+				//
+				put(map = ObjectUtils.getIfNull(map, LinkedHashMap::new), "", "");
+				//
+			} else if (s != null && s.length() == 2 && s.charAt(0) == '=') {
+				//
+				put(map = ObjectUtils.getIfNull(map, LinkedHashMap::new), "", s.substring(1, s.length()));
+				//
+			} else if (s != null && s.length() == 2 && s.charAt(s.length() - 1) == '=') {
+				//
+				put(map = ObjectUtils.getIfNull(map, LinkedHashMap::new), s.substring(0, s.length() - 1), "");
+				//
+			} else if (s != null && s.indexOf('=') >= 0 && s.indexOf('=') == s.lastIndexOf('=')) {
+				//
+				put(map = ObjectUtils.getIfNull(map, LinkedHashMap::new), StringUtils.substringBefore(s, '='),
+						StringUtils.substringAfter(s, '='));
+				//
+			} else if (s != null && s.length() > 2 && s.indexOf('=') != s.lastIndexOf('=')) {
+				//
+				put(map = ObjectUtils.getIfNull(map, LinkedHashMap::new), StringUtils.substring(s, 0, s.indexOf('=')),
+						StringUtils.substring(s, s.indexOf('=') + 1));
+				//
+			} // if
+				//
+		} // for
+			//
+		return map;
+		//
+	}
+
+	private static <K, V> void put(final Map<K, V> instance, final K key, final V value) {
+		if (instance != null) {
+			instance.put(key, value);
+		}
+	}
+
+	private static int length(final Object[] instance) {
+		return instance != null ? instance.length : 0;
+	}
+
+}
---
+++ src/test/java/org/apache/commons/lang3/UpdateVersionTest.java
@@ -0,0 +1,225 @@
+package org.apache.commons.lang3;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Member;
+import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Objects;
+
+import javax.xml.stream.XMLStreamException;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.google.common.reflect.Reflection;
+
+import io.github.toolfactory.narcissus.Narcissus;
+
+public class UpdateVersionTest {
+
+	private static class IH implements InvocationHandler {
+
+		private Boolean contains;
+
+		@Override
+		public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
+			//
+			final String name = getName(method);
+			//
+			if (proxy instanceof Collection && Objects.equals(name, "contains")) {
+				//
+				return contains;
+				//
+			} else if (proxy instanceof Map && Objects.equals(name, "put")) {
+				//
+				return null;
+				//
+			} // if
+				//
+			throw new Throwable(name);
+			//
+		}
+
+	}
+
+	@Test
+	void testNull() {
+		//
+		final Method[] ms = UpdateVersion.class.getDeclaredMethods();
+		//
+		Method m = null;
+		//
+		Object result = null;
+		//
+		String toString = null;
+		//
+		for (int i = 0; ms != null && i < ms.length; i++) {
+			//
+			if ((m = ArrayUtils.get(ms, i)) == null || m.isSynthetic()) {
+				//
+				continue;
+				//
+			} // if
+				//
+			toString = Objects.toString(m);
+			//
+			result = Narcissus.invokeStaticMethod(m, toArray(Collections.nCopies(m.getParameterCount(), null)));
+			//
+			if (contains(Arrays.asList(Integer.TYPE, Boolean.TYPE), m.getReturnType())) {
+				//
+				Assert.assertNotNull(result, toString);
+				//
+			} else {
+				//
+				Assert.assertNull(result, toString);
+				//
+			} // if
+				//
+		} // for
+			//
+	}
+
+	private static boolean contains(final Collection<?> items, final Object item) {
+		return items != null && items.contains(item);
+	}
+
+	private static Object[] toArray(final Collection<?> instance) {
+		return instance != null ? instance.toArray() : null;
+	}
+
+	@Test
+	void testNotNull() {
+		//
+		final Method[] ms = UpdateVersion.class.getDeclaredMethods();
+		//
+		Method m = null;
+		//
+		Object result = null;
+		//
+		String toString = null;
+		//
+		Class<?>[] parameterTypes = null;
+		//
+		Class<?> parameterType = null;
+		//
+		Collection<Object> collection = null;
+		//
+		final IH ih = new IH();
+		//
+		ih.contains = Boolean.FALSE;
+		//
+		for (int i = 0; ms != null && i < ms.length; i++) {
+			//
+			if ((m = ArrayUtils.get(ms, i)) == null || m.isSynthetic()
+					|| (parameterTypes = m.getParameterTypes()) == null
+					|| Boolean.logicalAnd(Objects.equals(getName(m), "toMap"),
+							Arrays.equals(parameterTypes, new Class[] { String[].class }))) {
+				//
+				continue;
+				//
+			} // if
+				//
+			clear(collection = ObjectUtils.getIfNull(collection, ArrayList::new));
+			//
+			for (int j = 0; j < parameterTypes.length; j++) {
+				//
+				if ((parameterType = ArrayUtils.get(parameterTypes, j)) != null && parameterType.isInterface()) {
+					//
+					add(collection, Reflection.newProxy(parameterType, ih));
+					//
+				} else if (Objects.equals(parameterType, String[].class)) {
+					//
+					add(collection, new String[] {});
+					//
+				} else if (parameterType != null && parameterType.isArray()) {
+					//
+					add(collection, Array.newInstance(parameterType, 0));
+					//
+				} else {
+					//
+					add(collection, Narcissus.allocateInstance(parameterType));
+					//
+				} // if
+					//
+			} // for
+				//
+			toString = Objects.toString(m);
+			//
+			result = Narcissus.invokeStaticMethod(m, toArray(collection));
+			//
+			if (contains(Arrays.asList(Integer.TYPE, Boolean.TYPE), m.getReturnType())) {
+				//
+				Assert.assertNotNull(result, toString);
+				//
+			} else {
+				//
+				Assert.assertNull(result, toString);
+				//
+			} // if
+				//
+		} // for
+			//
+	}
+
+	private static String getName(final Member instance) {
+		return instance != null ? instance.getName() : null;
+	}
+
+	private static <E> void add(final Collection<E> instance, final E item) {
+		if (instance != null) {
+			instance.add(item);
+		}
+	}
+
+	private static void clear(final Collection<?> instance) {
+		if (instance != null) {
+			instance.clear();
+		}
+	}
+
+	@Test
+	public void testMain() throws XMLStreamException, IOException, IllegalAccessException {
+		//
+		UpdateVersion.main(new String[] { "=", "= ", " =", "== ", "file=" });
+		//
+		UpdateVersion.main(new String[] { "file=pom.xml" });
+		//
+		UpdateVersion.main(new String[] { "file=pom.xml", "groupId=org.testng", "artifactId=testng" });
+		//
+		final File file = File.createTempFile(RandomStringUtils.secure().nextAlphanumeric(3), ".xml");
+		//
+		if (file != null) {
+			//
+			file.deleteOnExit();
+			//
+		} // if
+			//
+		final Path path = file != null ? file.toPath() : null;
+		//
+		Files.writeString(path,
+				"<project><dependencies><dependency><groupId>org.apache.commons</groupId><artifactId>commons-lang3</artifactId><version>3.19.0</version><scope></scope><exclusions></exclusions></dependency></dependencies></project>");
+		//
+		UpdateVersion.main(new String[] { "file=" + (file != null ? file.getAbsolutePath() : null),
+				"groupId=org.apache.commons", "artifactId=commons-lang3", "version=3.20.0" });
+		//
+		Assert.assertEquals(Files.readString(path),
+				"<project><dependencies><dependency><groupId>org.apache.commons</groupId><artifactId>commons-lang3</artifactId><version>3.20.0</version><scope></scope><exclusions></exclusions></dependency></dependencies></project>");
+		//
+		if (file != null) {
+			//
+			file.delete();
+			//
+		} // if
+			//
+	}
+
+}
